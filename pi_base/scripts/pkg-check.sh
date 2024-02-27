6d9e31491ed9ca490783cf3015908585637d2055
Ilya <iva2k@yahoo.com>
2024-02-26 23:09:45 -0800

    Add package.json for collection of shell commands * Add pi_base/scripts/pkg-check.sh to verify python package data files.

---
+++ pi_base/scripts/pkg-check.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Compare python package (extracted) to the original files, excluding:
+#  - *.md
+# 
+SOURCE=pi_base
+# TODO: (now) Auto-extract version and set $BUILD correctly:
+BUILD=dist/pi_base-0.0.1/pi_base
+
+all_diffs=$(
+  diff -u \
+    <(cd "$SOURCE" && find . -type f ! -name '*.py[cod]' ! -name '*.md' ! -path '*/__pycache__/*' ! -path '*/pictures*' ! -path '*/scripts/*' ! -path '*/tests/*' ! -path '*/remoteiot.com/*' | sort) \
+    <(cd "$BUILD"  && find . -type f ! -name '*.py[cod]' ! -name '*.md' ! -path '*/__pycache__/*' | sort) \
+  | grep -v -e '^---' | grep -v -e '^+++'
+)
+diffs=$(
+  echo "$all_diffs" \
+  | grep -E '^([-+])'
+  # | grep -E '^([<>]|---)'
+)
+diffs_cnt=$(echo -n "$diffs" | wc -l)
+
+if [ "$diffs_cnt" -eq 0 ]; then 
+  echo "Package content matches sources"
+else
+  echo "Error: found $diffs_cnt file(s) different:" >&2
+  echo "'$diffs'" >&2
+fi
+exit "$diffs_cnt"
