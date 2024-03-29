dd85a2937719583711697a7070cf9e4cadc23dfd
Ilya <iva2k@yahoo.com>
2024-03-28 21:39:01 -0700

    Back to development: 0.0.24

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## 0.0.24 (unreleased)
+
+
+- Nothing changed yet.
+
+
 ## 0.0.23 (2024-03-28)
 
 * Fix Loggr.print() log to journal args
--- pi_base/_version.py
+++ pi_base/_version.py
@@ -5,7 +5,7 @@ import sys
 
 
 # Do not edit this line:
-__version__ = "0.0.23"
+__version__ = "0.0.24.dev0"
 # instead, use commands from zest.releaser
 
 
